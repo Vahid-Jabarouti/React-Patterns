5ede48d6fece5b08cf0b3061ec5c4043c282e96c
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-08-27 17:58:49 +0430

    Adding props

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const items = [
+const secretItems = [
     "2 lb salmon",
     "5 springs fresh rosemary",
     "2 tablespoon olive oil",
@@ -7,7 +7,7 @@ const items = [
     "4 cloves ofchoped garlic"
   ];
 
-  const instructions = [
+  const secretInstructions = [
     "Preheat the oven to 375 degrees.",
     "Lightly coat aluminum foil with oil",
     "Place salmon on foil",
@@ -16,7 +16,7 @@ const items = [
     "Remove from oven."
   ]
 
-  function IngredientsList() {
+  function IngredientsList(props) {
     return (
       React.createElement(
     "section",
@@ -26,11 +26,11 @@ const items = [
       "div",
       {className: "ingredients"},
       React.createElement("ul", null,
-        items.map((ingredient, idx) => React.createElement("li", {key: idx}, ingredient)))
+        props.items.map((ingredient, idx) => React.createElement("li", {key: idx}, ingredient)))
     )))
   }
 
-  function InstructionsList() {
+  function InstructionsList(props) {
     return (
     React.createElement(
       "section",
@@ -38,7 +38,7 @@ const items = [
       React.createElement("h2", null, "Cooking Instructions"),
       React.createElement("div", null, React.createElement("div", 
       null,
-      instructions.map((ingredient, idx) => React.createElement("p", {key: idx}, ingredient)))))
+      props.instructions.map((ingredient, idx) => React.createElement("p", {key: idx}, ingredient)))))
     )
   }
 
@@ -60,4 +60,8 @@ const items = [
   //     null,
   //     instructions.map((ingredient, idx) => React.createElement("p", {key: idx}, ingredient))))))
 
-  ReactDOM.render([IngredientsList(), InstructionsList()], document.getElementById('root'))
+  ReactDOM.render(
+    [React.createElement(IngredientsList, {items: secretItems, key: 0}, null),
+     React.createElement(InstructionsList, {instructions: secretInstructions, key: 1}), null
+    ]
+    , document.getElementById('root'));
